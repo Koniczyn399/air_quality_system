77f8e1809b4c9d886810e398396c875469b22bc8
Koniczyn399 <supernk100@gmail.com>
2025-06-16 13:00:59 +0200

    Konwersja formularza urządzeń do livewire  (Szymon)

---
+++ resources/views/measurement-devices/form.blade.php
@@ -0,0 +1,14 @@
+<x-app-layout>
+    <div class="container mx-auto px-4 py-8 theme-container">
+        
+                @if (isset($measurementDevice->id)) 
+                    <h1 class="text-2xl font-bold mb-6 theme-text">Edytuj urządzenie: {{ $measurementDevice->name }}</h1>
+                    <livewire:measurement-devices.measurement-device-form :measurementDevice="$measurementDevice"  />
+                @else
+                    <h1 class="text-2xl font-bold mb-6 theme-text">Dodaj nowe urządzenie</h1>
+                    <livewire:measurement-devices.measurement-device-form />
+                @endif
+
+
+    </div>
+</x-app-layout>
